02c3f9eb7bd00df3fe9e11326f30913cf49d41d3
Pedro <pedrohalmeidamendonca@gmail.com>
2022-09-29 20:15:20 -0300

    Requisitos 6, 7 e 15

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,24 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Implemente uma função que retorna informações referentes aos elefantes conforme o argumento passado', () => {
+    expect(handlerElephants('count')).toBe(4);
+    expect(handlerElephants('names')).toEqual([
+      'Ilana',
+      'Orval',
+      'Bea',
+      'Jefferson',
+    ]);
+    expect(handlerElephants('averageAge')).toBe(10.5);
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
+    expect(handlerElephants('availability')).toEqual([
+      'Friday',
+      'Saturday',
+      'Sunday',
+      'Tuesday',
+    ]);
+    expect(handlerElephants()).toBe(undefined);
+    expect(handlerElephants({})).toBe('Parâmetro inválido, é necessário uma string');
+    expect(handlerElephants('car')).toBe(null);
+  });
+});
