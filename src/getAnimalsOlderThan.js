35537b4d1131198b51171fb032606c0730965d35
Pedro <pedrohalmeidamendonca@gmail.com>
2022-09-29 17:04:53 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  return data.species
+    .find((specie) => specie.name === animal)
+    .residents.every((resident) => resident.age >= age);
 }
 
+console.log(getAnimalsOlderThan('bears', 2));
 module.exports = getAnimalsOlderThan;
