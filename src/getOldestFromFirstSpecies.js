6a54a7bbaa3e5b3ffc64f7e1b556401b2aad81a5
Pedro <pedrohalmeidamendonca@gmail.com>
2022-09-30 18:14:38 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const oldestAnimal = data.species.find((element) => element.id === data.employees
+    .find((element2) => element2.id === id).responsibleFor[0]).residents
+    .map((element) => element.age).reduce((bigger, num) => ((bigger > num) ? bigger : num));
+  return Object.values(data.species.find((element) => element.id === data.employees
+    .find((element2) => element2.id === id).responsibleFor[0]).residents
+    .find((element) => element.age === oldestAnimal));
 }
 
 module.exports = getOldestFromFirstSpecies;
