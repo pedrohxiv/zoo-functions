bf3ff5aedb60607f5c5abe6afde7a447c0a61e08
Pedro <pedrohalmeidamendonca@gmail.com>
2022-09-29 22:52:22 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,20 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  return {
+    child: entrants.filter((element) => element.age < 18).length,
+    adult: entrants.filter((element) => element.age >= 18 && element.age < 50).length,
+    senior: entrants.filter((element) => element.age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  return entrants === undefined || Object.keys(entrants).length === 0
+    ? 0
+    : countEntrants(entrants).child * data.prices.child
+    + countEntrants(entrants).adult * data.prices.adult
+    + countEntrants(entrants).senior * data.prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
